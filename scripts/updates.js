9e57a5f8de4160f24f9108949c428f1a6c643790
GregoryPrograms <greg.dost@yahoo.com>
2018-03-17 10:47:22 -0700

    Completed filtering in update.js (now, just need nesting). As the columns start out being ordered by country, all parameters for globe should be ready for passing

--- scripts/updates.js
+++ scripts/updates.js
@@ -22,62 +22,90 @@ function update(filterArray, lastFilter){
 //First, we load in our csv data.
   d3.csv("data.csv", function(data) {
 //Now, we loop through and apply our filters. First, we create an unfiltered copy of the csv, to apply our filters to.
-fullFiltData = data;
-        for(iter of filterArray)
-	   {
+  fullFiltData = data;
+    for(iter of filterArray){
 //If the filter we're currently looking at is active.
-           if(iter.filtVal != "")
-             {
+      if(iter.filtVal != ""){
 //If the filter we're looking at is not color or shape (those need special parsing)
-             if(iter.key != "color" & iter.key != "shape")
-	       {
-               if(iter.key == "religion")
-                 {
-                 fullFiltData = fullFiltData.filter(function(d){return d.religion == iter.filtVal});
-                 }
-               else if(iter.key == "landmass")
-                 {
-                 fullFiltData = fullFiltData.filter(function(d){return d.landmass == iter.filtVal});
-                 }
-               else if(iter.key == "language")
-                 {
-                 fullFiltData = fullFiltData.filter(function(d){return d.language == iter.filtVal});
-                 }
-               }
-             else if(iter.key == "color")
-	       {
-               if(iter.filtVal == "red")
-		 {
-                 fullFiltData = fullFiltData.filter(function(d){return d.red == 1});
-                 }
-	        else if(iter.filtVal == "green")
-                 {
-                 fullFiltData = fullFiltData.filter(function(d){return d.green == 1});
-                 }
-                else if(iter.filtVal == "blue")
-                 {
-                 fullFiltData = fullFiltData.filter(function(d){return d.blue == 1});
-                 }
-                 else if(iter.filtVal == "gold")
-                 {
-                 fullFiltData = fullFiltData.filter(function(d){return d.gold == 1});
-                 }
-                 else if(iter.filtVal == "white")
-                 {
-                 fullFiltData = fullFiltData.filter(function(d){return d.white == 1});
-                 }
-                 else if(iter.filtVal == "black")
-                 {
-                 fullFiltData = fullFiltData.filter(function(d){return d.black == 1});
-                 }
-		 else if(iter.filtVal == "orange")
-                 {
-                 fullFiltData = fullFiltData.filter(function(d){return d.orange == 1});
-                 }
+        if(iter.key != "color" & iter.key != "shape"){
+          if(iter.key == "religion"){
+            fullFiltData = fullFiltData.filter(function(d){return d.religion == iter.filtVal});
+            }
+          else if(iter.key == "landmass"){
+            fullFiltData = fullFiltData.filter(function(d){return d.landmass == iter.filtVal});
+            }
+          else if(iter.key == "language"){
+            fullFiltData = fullFiltData.filter(function(d){return d.language == iter.filtVal});
+            }
+          }
+        else if(iter.key == "color"){
+          if(iter.filtVal == "red"){
+            fullFiltData = fullFiltData.filter(function(d){return d.red == 1});
+            }
+	  else if(iter.filtVal == "green"){
+            fullFiltData = fullFiltData.filter(function(d){return d.green == 1});
+            }
+          else if(iter.filtVal == "blue"){
+            fullFiltData = fullFiltData.filter(function(d){return d.blue == 1});
+            }
+          else if(iter.filtVal == "gold"){
+            fullFiltData = fullFiltData.filter(function(d){return d.gold == 1});
+            }
+          else if(iter.filtVal == "white"){
+            fullFiltData = fullFiltData.filter(function(d){return d.white == 1});
+            }
+          else if(iter.filtVal == "black"){
+            fullFiltData = fullFiltData.filter(function(d){return d.black == 1});
+            }
+          else if(iter.filtVal == "orange"){
+            fullFiltData = fullFiltData.filter(function(d){return d.orange == 1});
+            }
+          }
+        else if(iter.key == "shape"){
+	  if(iter.filtVal == "bars"){
+            fullFiltData = fullFiltData.filter(function(d){return d.bars > 0});
+            }
+	  if(iter.filtVal == "stripes"){
+            fullFiltData = fullFiltData.filter(function(d){return d.stripes > 0});
+            }
+          if(iter.filtVal == "circles"){
+            fullFiltData = fullFiltData.filter(function(d){return d.circles > 0});
+            }
+          if(iter.filtVal == "crosses"){
+            fullFiltData = fullFiltData.filter(function(d){return d.crosses > 0});
+            }
+          if(iter.filtVal == "saltires"){
+            fullFiltData = fullFiltData.filter(function(d){return d.saltires > 0});
+            }
+          if(iter.filtVal == "sunstars"){
+            fullFiltData = fullFiltData.filter(function(d){return d.sunstars > 0});
+            }          
+          if(iter.filtVal == "crescent"){
+            fullFiltData = fullFiltData.filter(function(d){return d.crescent == 1});
+            } 
+          if(iter.filtVal == "triangle"){
+            fullFiltData = fullFiltData.filter(function(d){return d.triangle == 1});
+            }	   
+          if(iter.filtVal == "icon"){
+            fullFiltData = fullFiltData.filter(function(d){return d.icon == 1});
+            } 
+          if(iter.filtVal == "animate"){
+            fullFiltData = fullFiltData.filter(function(d){return d.animate == 1});
+            } 
+          if(iter.filtVal == "text"){
+            fullFiltData = fullFiltData.filter(function(d){return d.text == 1});
+            }
 
 
-               }
-             } 
-           }
-	}  
+
+
+
+
+
+
+
+          }
+       } 
+    }  
+  }
 }
