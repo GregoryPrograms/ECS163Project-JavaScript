2099a909bfce7759d3d534d78e5571c4a4426e39
GregoryPrograms <greg.dost@yahoo.com>
2018-03-19 22:01:32 -0700

    Finished all code for updates and filters (for now, just waiting for vis functions to pass stuff to in order to test

--- scripts/updates.js
+++ scripts/updates.js
@@ -23,6 +23,7 @@ function update(filterArray, lastFilter){
   d3.csv("data.csv", function(data) {
 //Now, we loop through and apply our filters. First, we create an unfiltered copy of the csv, to apply our filters to.
   fullFiltData = data;
+  filtButLast = data;
     for(iter of filterArray){
 //If the filter we're currently looking at is active.
 	 if(iter.filtVal != ""){
@@ -30,96 +31,118 @@ function update(filterArray, lastFilter){
             case "religion":
               fullFiltData = fullFiltData.filter(function(d){
 		            return d.religion == iter.filtVal});
+              if(iter.key != lastFilter){filtButLast = filtButLast.filter(function(d){return d.religion == iter.filtVal})};
               break;
             case "landmass":
               fullFiltData = fullFiltData.filter(function(d){
 		            return d.landmass == iter.filtVal});
+              if(iter.key != lastFilter){filtButLast = filtButLast.filter(function(d){return d.landmass == iter.filtVal})};
               break;
             case "language":
               fullFiltData = fullFiltData.filter(function(d){
 	              return d.language == iter.filtVal});
+              if(iter.key != lastFilter){filtButLast = filtButLast.filter(function(d){return d.language == iter.filtVal})};
               break;
             case "color":
             case "shape":
               switch(iter.filtVal){
           	    case "red":
-                  fullFiltData = fullFiltData.filter(function(d){
-                    return d.red == 1});
-                  break;
-	              case "green":
-                  fullFiltData = fullFiltData.filter(function(d){
-                    return d.green == 1});
-                  break;
-                case "blue":
-                  fullFiltData = fullFiltData.filter(function(d){
-		                return d.blue == 1});
-                  break;
-                case "gold":
-                  fullFiltData = fullFiltData.filter(function(d){
-	                  return d.gold == 1});
-                  break;
-                case "white":
-                  fullFiltData = fullFiltData.filter(function(d){
-		                return d.white == 1});
-                  break;
-                case "black":
-                  fullFiltData = fullFiltData.filter(function(d){
-		                return d.black == 1});
-                  break;
-                case "orange":
-                  fullFiltData = fullFiltData.filter(function(d){
-		                return d.orange == 1});
-                  break;
-	              case "bars":
-                  fullFiltData = fullFiltData.filter(function(d){
-		                return d.bars > 0});
-                  break;
-	              case "stripes":
-                  fullFiltData = fullFiltData.filter(function(d){
-                    return d.stripes > 0});
-                  break;
-                case "circles":
-                  fullFiltData = fullFiltData.filter(function(d){
-		                return d.circles > 0});
-                  break;
-                case "crosses":
-                  fullFiltData = fullFiltData.filter(function(d){
-                    return d.crosses > 0});
-                  break;
-                case "saltires":
-                  fullFiltData = fullFiltData.filter(function(d){
-                    return d.saltires > 0});
-		              break;
-                case "sunstars":
-                  fullFiltData = fullFiltData.filter(function(d){
-                    return d.sunstars > 0});
-                  break;
-                case "crescent":
-                  fullFiltData = fullFiltData.filter(function(d){
-		                return d.crescent == 1});
-                  break;
-                case "triangle":
-                  fullFiltData = fullFiltData.filter(function(d){
-		                return d.triangle == 1});
-		              break;
-                case "icon":
-                  fullFiltData = fullFiltData.filter(function(d){
-		                return d.icon == 1});
-                  break;
-                case "animate":
+                      fullFiltData = fullFiltData.filter(function(d){
+                        return d.red == 1});
+              	      if(iter.key != lastFilter){filtButLast = filtButLast.filter(function(d){return d.red == 1})};
+                      break;
+	            case "green":
+                      fullFiltData = fullFiltData.filter(function(d){
+                        return d.green == 1});
+                        if(iter.key != lastFilter){filtButLast = filtButLast.filter(function(d){return d.green == 1})};
+                      break;
+                    case "blue":
+                      fullFiltData = fullFiltData.filter(function(d){
+		        return d.blue == 1});
+                      if(iter.key != lastFilter){filtButLast = filtButLast.filter(function(d){return d.blue == 1})};
+                      break;
+                    case "gold":
+                      fullFiltData = fullFiltData.filter(function(d){
+	                return d.gold == 1});
+                      if(iter.key != lastFilter){filtButLast = filtButLast.filter(function(d){return d.gold == 1})};
+                      break;
+                    case "white":
+                      fullFiltData = fullFiltData.filter(function(d){
+		        return d.white == 1});
+                      if(iter.key != lastFilter){filtButLast = filtButLast.filter(function(d){return d.white == 1})};
+                      break;
+                    case "black":
+                      fullFiltData = fullFiltData.filter(function(d){
+		        return d.black == 1});
+                      if(iter.key != lastFilter){filtButLast = filtButLast.filter(function(d){return d.black == 1})};
+                      break;
+                    case "orange":
+                      fullFiltData = fullFiltData.filter(function(d){
+		        return d.orange == 1});
+                      if(iter.key != lastFilter){filtButLast = filtButLast.filter(function(d){return d.orange == 1})};
+                      break;
+	            case "bars":
+                      fullFiltData = fullFiltData.filter(function(d){
+		        return d.bars > 0});
+                      if(iter.key != lastFilter){filtButLast = filtButLast.filter(function(d){return d.bars > 0})};
+                      break;
+	            case "stripes":
+                      fullFiltData = fullFiltData.filter(function(d){
+                        return d.stripes > 0});
+                      if(iter.key != lastFilter){filtButLast = filtButLast.filter(function(d){return d.stripes > 0})};
+                      break;
+                    case "circles":
+                      fullFiltData = fullFiltData.filter(function(d){
+                        return d.circles > 0});
+                      if(iter.key != lastFilter){filtButLast = filtButLast.filter(function(d){return d.circles > 0})};
+                      break;
+                   case "crosses":
+                     fullFiltData = fullFiltData.filter(function(d){
+                       return d.crosses > 0});
+                     if(iter.key != lastFilter){filtButLast = filtButLast.filter(function(d){return d.crosses > 0})};
+                     break;
+                   case "saltires":
+                     fullFiltData = fullFiltData.filter(function(d){
+                       return d.saltires > 0});
+                     if(iter.key != lastFilter){filtButLast = filtButLast.filter(function(d){return d.saltires > 0})};
+                     break;
+                   case "sunstars":
+                     fullFiltData = fullFiltData.filter(function(d){
+                       return d.sunstars > 0});
+                     if(iter.key != lastFilter){filtButLast = filtButLast.filter(function(d){return d.sunstars > 0})};
+                     break;
+                   case "crescent":
+                     fullFiltData = fullFiltData.filter(function(d){
+                       return d.crescent == 1});
+                     if(iter.key != lastFilter){filtButLast = filtButLast.filter(function(d){return d.crescent > 0})};
+                     break;
+                   case "triangle":
+                     fullFiltData = fullFiltData.filter(function(d){
+                       return d.triangle == 1}); 
+                     if(iter.key != lastFilter){filtButLast = filtButLast.filter(function(d){return d.triangle == 1})};             
+ 	             break;
+                   case "icon":
+                     fullFiltData = fullFiltData.filter(function(d){
+		       return d.icon == 1});
+                     if(iter.key != lastFilter){filtButLast = filtButLast.filter(function(d){return d.icon == 1})};
+                     break;
+                  case "animate":
             	    fullFiltData = fullFiltData.filter(function(d){
-		                return d.animate == 1});
-                  break;
-                case "text":
-                  fullFiltData = fullFiltData.filter(function(d){
-		                return d.text == 1});
-                  break;
-                default: break;
+		      return d.animate == 1});
+                    if(iter.key != lastFilter){filtButLast = filtButLast.filter(function(d){return d.animate == 1})};
+                    break;
+                  case "text":
+                    fullFiltData = fullFiltData.filter(function(d){
+                      return d.text == 1});
+                    if(iter.key != lastFilter){filtButLast = filtButLast.filter(function(d){return d.text == 1})};
+                    break;
+                  default: break;
                 }//Switch iter.filtVal (for color and shape)
-              default: break;
+                default: break;
               }//Switch iter.key(overall switch)
-           }//if(iter.filtVal == " ")
+           }//if(iter.filtVal != " ")
         }//for(iter of filterArray)
-     console.log(fullFiltData);
-   });//d3.csv()
-  }//function update()
+        //This is where graph calls go.   
+      });//d3.csv()
+    }//function update()
+
